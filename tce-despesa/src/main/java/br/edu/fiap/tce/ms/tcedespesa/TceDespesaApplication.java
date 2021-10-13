c1b888fb06d775c66b4a56092e4214a778e572d3
Tiago Kazuo Lino <tiagokazuolino@hotmail.com>
2021-10-13 20:03:23 -0300

    MS Despesa Project is Created

---
+++ tce-despesa/src/main/java/br/edu/fiap/tce/ms/tcedespesa/TceDespesaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fiap.tce.ms.tcedespesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TceDespesaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TceDespesaApplication.class, args);
+    }
+
+}
---
+++ tce-despesa/src/test/java/br/edu/fiap/tce/ms/tcedespesa/TceDespesaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fiap.tce.ms.tcedespesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TceDespesaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
