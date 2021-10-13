23c38f84a9668d471168bfb40110e9e391e73a56
Tiago Kazuo Lino <tiagokazuolino@hotmail.com>
2021-10-13 20:03:06 -0300

    MS Cadastro Project is Created

---
+++ tce-cadastro/src/main/java/br/edu/fiap/tce/ms/tcecadastro/TceCadastroApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fiap.tce.ms.tcecadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TceCadastroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TceCadastroApplication.class, args);
+    }
+
+}
---
+++ tce-cadastro/src/test/java/br/edu/fiap/tce/ms/tcecadastro/TceCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fiap.tce.ms.tcecadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TceCadastroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
