0e408390557fda97e8729636cf3f9b239fa258cf
Tiago Kazuo Lino <tiagokazuolino@hotmail.com>
2021-10-13 20:13:46 -0300

    MS Validador Project is Created

---
+++ tce-validador/src/main/java/br/edu/fiap/tce/ms/tcevalidador/TceValidadorApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fiap.tce.ms.tcevalidador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TceValidadorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TceValidadorApplication.class, args);
+    }
+
+}
---
+++ tce-validador/src/test/java/br/edu/fiap/tce/ms/tcevalidador/TceValidadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fiap.tce.ms.tcevalidador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TceValidadorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
